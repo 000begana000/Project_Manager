6ceca773629f2738b7e0f5d321cd68f5d00e66b4
000begana000 <begana.contact@gmail.com>
2025-03-18 12:27:44 +0100

    create sidebar

---
+++ src/components/NoProject.jsx
@@ -0,0 +1,7 @@
+export default function NoProject() {
+  return (
+    <div>
+      <h1 className="font-headline">No Project</h1>
+    </div>
+  );
+}
---
+++ src/components/ProjectSidebar.jsx
@@ -0,0 +1,8 @@
+export default function ProjectSidebar() {
+  return (
+    <div className="project-sidebar">
+      <h1 className="font-headline">your project</h1>
+      <button className="font-button button-purple">+ Add Project</button>
+    </div>
+  );
+}
