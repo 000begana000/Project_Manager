0467244ffd6233bcde57504c3fcf94fe10fccdcf
000begana000 <begana.contact@gmail.com>
2025-03-19 09:49:27 +0100

    feat: validation and warning modal

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,16 +2,28 @@ import { useRef } from "react";
 
 import Button from "./UI/Button";
 import Input from "./UI/Input";
+import Modal from "./UI/Modal";
 
 export default function NewProject({ onCancel, onAdd }) {
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
+  const dialog = useRef();
 
   function handleSave() {
     const enteredTitle = title.current.value;
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
+
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      //modal
+      dialog.current.open();
+      return;
+    }
 
     onAdd({
       title: enteredTitle,
@@ -21,24 +33,29 @@ export default function NewProject({ onCancel, onAdd }) {
   }
 
   return (
-    <div className="content new-project">
-      <menu className="new-project-buttons">
-        <li>
-          <Button transparant onClick={onCancel}>
-            Cancel
-          </Button>
-        </li>
-        <li>
-          <Button black onClick={handleSave}>
-            Save
-          </Button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" label="Title" ref={title} />
-        <Input textarea label="Description" ref={description} />
-        <Input type="date" label="Due Date" ref={dueDate} />
+    <>
+      <Modal ref={dialog} buttonCaption="Close">
+        <p>something went wrong</p>
+      </Modal>
+      <div className="content new-project">
+        <menu className="new-project-buttons">
+          <li>
+            <Button transparant onClick={onCancel}>
+              Cancel
+            </Button>
+          </li>
+          <li>
+            <Button black onClick={handleSave}>
+              Save
+            </Button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" label="Title" ref={title} />
+          <Input textarea label="Description" ref={description} />
+          <Input type="date" label="Due Date" ref={dueDate} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,26 @@
+import { useImperativeHandle, useRef, ref } from "react";
+import { createPortal } from "react-dom";
+
+import Button from "./Button";
+
+export default function Modal({ children, buttonCaption, ref }) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog ref={dialog}>
+      {children}
+      <form method="dialog">
+        <Button>{buttonCaption}</Button>
+      </form>
+    </dialog>,
+    document.getElementById("modal-root")
+  );
+}
