52e125e0033262347be34b415fc06a20d7ff3f2e
000begana000 <begana.contact@gmail.com>
2025-03-25 13:46:12 +0100

    feat: delete project

--- src/App.jsx
+++ src/App.jsx
@@ -54,13 +54,27 @@ function App() {
     console.log(projectsState.selectedProjectId);
   }
 
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          project => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
+
   const selectedProject = projectsState.projects.find(
     project => project.id === projectsState.selectedProjectId
   );
 
   console.log(projectsState.projects);
 
-  let content = <SelectedProject project={selectedProject} />;
+  let content = (
+    <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+  );
 
   if (projectsState.selectedProjectId === undefined) {
     content = <NoProject onStartAddProject={handleStartAddProject} />;
--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,6 +1,6 @@
 import Button from "./UI/Button";
 
-export default function SelectedProject({ project }) {
+export default function SelectedProject({ project, onDelete }) {
   const formattedDate = new Date(project.dueDate).toLocaleDateString("en-US", {
     year: "numeric",
     month: "short",
@@ -11,10 +11,13 @@ export default function SelectedProject({ project }) {
     <div className="content selected-project">
       <div>
         <h1 className="font-headline title">{project.title}</h1>
-        <Button transparant>Delete</Button>
+        <Button transparant onClick={onDelete}>
+          Delete
+        </Button>
       </div>
       <p className="font-paragraph date">{formattedDate}</p>
       <p className="font-paragraph description">{project.description}</p>
+      <h1>Tasks (update later)</h1>
     </div>
   );
 }
